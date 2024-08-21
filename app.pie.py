d97bdb6fb62f5418b61ffccf744b7d768ddd8e94
sun496 <eunsun070810@gmail.com>
2024-08-21 16:34:35 +0900

    Update app.pie.py

--- app.pie.py
+++ app.pie.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.header("Hello")
 st.header("소윤")
 st.write("정연이랑 같이 놀자")
+import streamlit as st
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
