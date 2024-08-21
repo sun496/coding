d389f1e983173c15195a233fd6bdac751e00f5a0
sun496 <eunsun070810@gmail.com>
2024-08-21 16:39:26 +0900

    Update app.pie.py

--- app.pie.py
+++ app.pie.py
@@ -1,12 +1,6 @@
-import streamlit as st
-
-st.header("Hello")
-st.header("소윤")
-st.write("정연이랑 같이 놀자")
-import streamlit as st
 
 st.title("은선이의 옷가게")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("_이 옷가게_ 는 :blue[cool] :sunglasses:")
 import streamlit as st
 
 sentiment_mapping = ["one", "two", "three", "four", "five"]
@@ -20,3 +14,10 @@ selected = st.feedback("thumbs")
 if selected is not None:
     st.markdown(f"You selected: {sentiment_mapping[selected]}")
 Copy
+import streamlit as st
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
